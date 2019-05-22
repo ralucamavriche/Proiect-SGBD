92925b3cd568998377d3b3cf2298e0235b45f0dc
Vatamanelu Andreea <andreea.vatamanelu@yahoo.com>
2019-05-22 07:56:41 +0300

    functie totalSumaComanda

--- functii/afisareProduseReduse.sql
+++ functii/afisareProduseReduse.sql
@@ -1,32 +1,52 @@
 CREATE OR REPLACE procedure afisare_produse_reduse
+(v_id IN INT, v_comanda IN VARCHAR2, v_raspuns out varchar2)
 AS 
     v_nume varchar2(60);
-    cursor c_id_reducere is SELECT id_reducere FROM produse where id_reducere is not null;
+    cursor c_id_reducere is SELECT id_reducere FROM produse where id_reducere is not null and id=v_id;
    vv_id int;
-    v_procent_reducere int;
-    v_id int;
     v_id_red int;
     v_procent int;
     v_pret int;
-    
+    v_id_produse int;
+    v_id_articol int;
+    v_numar_bucati int;
     v_pret_redus int;
 
 BEGIN
    open c_id_reducere;
-LOOP
    fetch c_id_reducere into vv_id;
-   exit when c_id_reducere%NOTFOUND;
 
-    select id,nume_produs,pret into v_id,v_nume,v_pret from produse where id=vv_id;
+    select id, id_articol, nume_produs,pret, numar_bucati into v_id_produse, v_id_articol, v_nume,v_pret, v_numar_bucati from produse where id=v_id;
     select id,procent_reducere into v_id_red,v_procent from reduceri where id=vv_id;
     v_pret_redus := v_pret- (v_procent/100)*v_pret;
-    dbms_output.put_line('Produsul ' || v_nume || ' cu id-ul ' || v_id || ' a ajuns de la suma de ' 
-    || v_pret || 'lei la suma de ' || v_pret_redus || 'lei dupa aplicarea reducerii de ' ||  v_procent || '%');
-END LOOP;
+    
+IF upper(v_comanda)='NUME_PRODUS' then
+        v_raspuns := v_nume;
+    end if;
+IF upper(v_comanda)='ID_ARTICOL' THEN
+        v_raspuns := v_id_articol;
+        end if;
+IF upper(v_comanda)='ID_REDUCERE' THEN
+        v_raspuns := v_id_red;
+        end if;
+IF upper(v_comanda)='PRET' THEN
+        v_raspuns := v_pret;
+        end if;
+IF upper(v_comanda)='NUMAR_BUCATI' THEN
+        v_raspuns := v_numar_bucati;
+        end if;
+IF upper(v_comanda)='PRET_REDUS' THEN
+        v_raspuns := v_pret_redus;
+        end if;
+        
 close c_id_reducere;
 END;
 
 set serveroutput on;
+declare 
+v_out varchar2(100);
 begin
-    afisare_produse_reduse();
+    afisare_produse_reduse(3619,'NUMAR_BUCATI',v_out);
+    DBMS_OUTPUT.PUT_LINE(v_out);
 end;
+
---
+++ functii/totalSumaComanda.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE function total_suma_comanda
+        (v_id IN INT) return number
+AS 
+    v_nume varchar2(60);
+    cursor c_id_comanda is SELECT id_comanda FROM detalii_comenzi where id_comanda=v_id;
+    cursor c_id_produs is SELECT id_produs FROM detalii_comenzi where id_comanda=v_id;
+    vv_id int;
+    v_nr_bucati int;
+    vv_id_produs int;
+    v_suma int;
+    v_pret int;
+
+BEGIN
+    suma:=0;
+   open c_id_comanda;
+   open c_id_produs;   
+LOOP
+    fetch c_id_produs into vv_id_produs;
+    exit when c_id_produs%NOTFOUND;
+     select nume_produs,pret into v_nume, v_pret from produse where vv_id_produs=id;
+     
+    fetch c_id_comanda into vv_id;
+    exit when c_id_comanda%NOTFOUND;
+    select numar_bucata_produs  into  v_nr_bucati from detalii_comenzi where id_comanda=vv_id and id_produs=vv_id_produs;
+
+    suma:= suma + v_pret*v_nr_bucati;
+END LOOP;
+close c_id_comanda;
+close c_id_produs;
+
+return suma;
+END total_suma_comanda;
+
+set serveroutput on;
+begin
+    total_suma_comanda(222222);
+end;
